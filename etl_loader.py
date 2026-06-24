87b6923ebead6d733f0292ae7d78533b184a2b06
KarakozN111 <156894696+KarakozN111@users.noreply.github.com>
2026-06-24 11:23:41 +0500

    Update etl_loader.py

--- etl_loader.py
+++ etl_loader.py
@@ -1,6 +1,6 @@
 import os #для работы с ос компа или контнейнера
 import pandas as pd #to work with dataframes
-from sqlalchemy import create_engine, text #
+from sqlalchemy import create_engine, text 
 
 # Full Refresh 
 def load_csv_to_raw_layer(file_path: str, table_name: str, db_url: str): 
@@ -42,11 +42,10 @@ def load_csv_to_raw_incremental(file_path: str, table_name: str, db_url: str):
         
     print(f"Последняя дата updated_at в базе для {table_name}: {max_updated_at}")
     
-    # 2. Читаем файл с оценками
     df = pd.read_csv(file_path)
     df['updated_at'] = pd.to_datetime(df['updated_at'])
     
-    # 3. Шаг инкремента: Если в базе уже есть записи, фильтруем только новые
+    # инкремент
     if max_updated_at is not None:
         max_updated_at = pd.to_datetime(max_updated_at)
         df_incremental = df[df['updated_at'] > max_updated_at]
@@ -54,7 +53,7 @@ def load_csv_to_raw_incremental(file_path: str, table_name: str, db_url: str):
         print("Таблица в базе пустая, загружаем файл полностью.")
         df_incremental = df
 
-    # 4. Записываем только дельту (новые строки), не затирая старое (if_exists='append')
+    # 4 записываем новые строки, не затирая старое
     if not df_incremental.empty:
         print(f"Найдено {len(df_incremental)} новых строк для загрузки.")
         with engine.begin() as connection:
@@ -69,31 +68,27 @@ def load_csv_to_raw_incremental(file_path: str, table_name: str, db_url: str):
     else:
         print(f"Новых данных нет. Таблица raw.{table_name} уже актуальна.\n")
 
-# 4 task: Функции для трансформации, проверки качества и публикации
-
+# 4 функции для трансформации, проверки качества и публикации
 def transform_raw_to_dwh_layer(db_url: str):
-    #Стадия TRANSFORM: Перенос данных из raw в dwh с трансформацией
+    # перенос данных из raw в dwh с трансформацией
     engine = create_engine(db_url)
     with engine.begin() as connection:
         print("Запуск трансформации данных и перенос в слой DWH...")
     print("Трансформация успешно завершена.\n")
 
 def run_data_quality_checks(db_url: str):
-    #Стадия QUALITY_CHECK: Проверка качества чистых данных в DWH
     engine = create_engine(db_url)
     with engine.connect() as connection:
         print("Запуск проверок качества данных (Data Quality Checks)...")
     print("Все проверки качества успешно пройдены.\n")
 
 def publish_to_mart_layer(db_url: str):
-    #Стадия PUBLISH: Расчет витрин данных в слое mart
     engine = create_engine(db_url)
     with engine.begin() as connection:
         print("Формирование и публикация витрин в слой mart...")
     print("Витрины успешно обновлены и опубликованы для аналитиков.\n")
 
 def log_dag_start(dag_id: str, run_id: str, db_url: str):
-    #Фиксирует начало работы пайплайна в статусной таблице
     engine = create_engine(db_url)
     with engine.begin() as connection:
         query = text("""
@@ -104,7 +99,6 @@ def log_dag_start(dag_id: str, run_id: str, db_url: str):
     print(f"Запись о начале прогона {run_id} добавлена в etl_dag_runs.")
 
 def log_dag_end(dag_id: str, run_id: str, status: str, db_url: str, error_msg: str = None):
-    #Фиксирует завершение работы пайплайна (SUCCESS или FAILED)
     engine = create_engine(db_url)
     with engine.begin() as connection:
         query = text("""
